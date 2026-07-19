9f6f7929f36d556414097b3aaba9f546f0526044
sabashahid786 <sabashahid4846@gmail.com>
2026-07-19 23:52:08 +0500

    first assignmnet

---
+++ assignment-1/app.js
@@ -0,0 +1,16 @@
+// Question-01
+alert("ASLAM O ALAIKUM");
+
+// Question-02
+alert("Alert! please enter a valid passord");
+
+// Question-03
+alert("Welcome to JS land...\n Happy coding")
+
+// Question-04
+alert("Welcome to JS land")
+alert("Prevent this page from creating additional dialogs")
+
+// Question-05 
+var idalert = "Hello... I can run JS through my web browser's console"
+console.log (idalert)
