74178153a097e359e2c68fe0b7259d88d9b6547a
sabashahid786 <sabashahid4846@gmail.com>
2026-07-20 01:35:21 +0500

    complete

---
+++ assignment-2/app.js
@@ -0,0 +1,48 @@
+// Question-01
+
+var username
+
+
+
+// Question-02
+var myName = " Saba Shahid";
+
+// Question-03
+
+ var message
+    message = "Hello World";
+
+    alert(message);
+
+
+    // Question-04
+     var studentName = "Saba Shahid";
+    var studentAge = "25 years old";
+    var studentCourse = "Certified mobile Application Development";
+
+     alert(studentName);
+    alert(studentAge);
+    alert(studentCourse);
+
+
+     // Question-05
+
+     var pizza ="PIZZA \n PIZZ \n PIZ \n PI \n P "
+
+     alert(pizza);
+
+       // Question-06
+
+       var email = "example@example.com";
+
+    alert("My email address is " + email);
+
+
+  // Question-07
+  var book = "A smarter way to learn JavaScript";
+
+    alert("I am trying to learn from the Book " + book);
+
+      // Question-08
+    
+      document.write ("Yeah! i can write HTML content through Java ")
---
+++ assignment-3/app.js
@@ -0,0 +1,29 @@
+// Question-01
+
+var age = 25;   
+
+    alert("I am " + age + " years old.");
+
+
+
+// Question-02
+     var visitCount = 20;
+
+    alert("You have visited this site " + visitCount + " times." );
+
+
+
+// Question-03
+    var birthYear = 2000;  
+
+    document.write( "My birth year is " + birthYear + "<br>");
+    document.write("Data type of my declared variable is number");
+
+
+     var visitorName = "John Doe";
+    var productTitle = "T-shirt";
+    var quantity = 5;
+
+    document.write("<br><br>");
+
+    document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
