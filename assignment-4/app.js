c6383fbbff49a7540573a68fc0f1bcb5e50de128
sabashahid786 <sabashahid4846@gmail.com>
2026-07-22 22:21:18 +0500

    final

---
+++ assignment-4/app.js
@@ -0,0 +1,38 @@
+// 1. Declare 3 variables in one statement
+
+var firstName, lastName, age;
+
+// 2. Declare 5 legal & 5 illegal variable names
+//  Legal Variable Names
+
+var name;
+var name1;
+var $name;
+var myName;
+var my_1stVariable;
+
+// Illegal Variable Names
+
+// var 1name;      
+// var my-name;   
+// var my name;    
+// var var;        
+// var @name;      
+
+
+// 3.
+// (a)
+  document.write("<h1>Rules for naming JS variables</h1>");
+
+// (b)
+
+document.write("Variable names can only contain letters, numbers, $ and _. For example: <b>$my_1stVariable</b><br><br>");
+// (c)
+    document.write("Variables must begin with a letter, $ or _. For example: <b>$name</b>, <b>_name</b> or <b>name</b><br><br>");
+// (d)
+    document.write("Variable names are case <b>sensitive</b>.<br><br>");
+// (e)
+    document.write("Variable names should not be JS <b>keywords</b>.");
+
+
+
---
+++ assignment-5/app.js
@@ -0,0 +1,139 @@
+ 
+//  question 1
+ 
+ 
+ var num1 = 3;
+    var num2 = 5;
+    var sum = num1 + num2;
+
+    document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+
+//  question 2
+
+    var subtraction = num1 - num2;
+    var multiplication = num1 * num2;
+    var division = num1 / num2;
+    var modulus = num1 % num2;
+
+    document.write("Subtraction of " + num1 + " and " + num2 + " is " + subtraction + "<br>");
+    document.write("Multiplication of " + num1 + " and " + num2 + " is " + multiplication + "<br>");
+    document.write("Division of " + num1 + " and " + num2 + " is " + division + "<br>");
+    document.write("Modulus of " + num1 + " and " + num2 + " is " + modulus);
+
+    //  question 3
+     // a. Declare a variable
+    var number;
+
+    // b. Show value after declaration
+    document.write("Value after variable declaration is: " + number + "<br><br>");
+
+    // c. Initialize the variable
+    number = 5;
+
+    // d. Show initial value
+    document.write("Initial value: " + number + "<br><br>");
+
+    // e. Increment the variable
+    number++;
+
+    // f. Show value after increment
+    document.write("Value after increment is: " + number + "<br><br>");
+
+    // g. Add 7 to the variable
+    number = number + 7;
+
+    // h. Show value after addition
+    document.write("Value after addition is: " + number + "<br><br>");
+
+    // i. Decrement the variable
+    number--;
+
+    // j. Show value after decrement
+    document.write("Value after decrement is: " + number + "<br><br>");
+
+    // k. Find remainder after dividing by 3
+    var remainder = number % 3;
+
+    // l. Show remainder
+    document.write("The remainder is: " + remainder);
+
+
+
+    // question-04
+    var ticketPrice = 600;
+    var totalCost = ticketPrice * 5;
+
+    document.write("Total cost to buy 5 tickets to a movie is " + totalCost + " PKR");
+
+
+
+    // question-05
+
+    var number = 2;
+
+    document.write("<h2>Table of 2</h2>");
+
+    document.write(number + " x 1 = " + (number * 1) + "<br>");
+    document.write(number + " x 2 = " + (number * 2) + "<br>");
+    document.write(number + " x 3 = " + (number * 3) + "<br>");
+    document.write(number + " x 4 = " + (number * 4) + "<br>");
+    document.write(number + " x 5 = " + (number * 5) + "<br>");
+    document.write(number + " x 6 = " + (number * 6) + "<br>");
+    document.write(number + " x 7 = " + (number * 7) + "<br>");
+    document.write(number + " x 8 = " + (number * 8) + "<br>");
+    document.write(number + " x 9 = " + (number * 9) + "<br>");
+    document.write(number + " x 10 = " + (number * 10));
+
+
+ // question-07
+
+     var priceItem1 = 650;
+    var priceItem2 = 100;
+    var quantityItem1 = 3;
+    var quantityItem2 = 7;
+    var shippingCharges = 100;
+
+    var totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
+
+    document.write("<h1>Shopping Cart</h1>");
+
+    document.write("Price of item 1 is " + priceItem1 + "<br>");
+    document.write("Quantity of item 1 is " + quantityItem1 + "<br>");
+    document.write("Price of item 2 is " + priceItem2 + "<br>");
+    document.write("Quantity of item 2 is " + quantityItem2 + "<br>");
+    document.write("Shipping Charges " + shippingCharges + "<br><br>");
+
+    document.write("Total cost of your order is " + totalCost);
+
+
+    // question-08
+      var totalMarks = 980;
+    var marksObtained = 804;
+
+    var percentage = (marksObtained / totalMarks) * 100;
+
+    document.write("<h1>Marks Sheet</h1>");
+    document.write("Total Marks: " + totalMarks + "<br>");
+    document.write("Marks Obtained: " + marksObtained + "<br>");
+    document.write("Percentage: " + percentage + "%");
+
+
+       // question-09
+        var total = (10 * 104.80) + (25 * 28);
+
+    document.write("<h1>Currency in PKR</h1>");
+    document.write("Total Currency in PKR: " + total);
+
+
+
+
+     // question-10
+     var number = 10;
+
+    var result = ((number + 5) * 10) / 2;
+
+    document.write("Result: " + result);
+
+
+      
