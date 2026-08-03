d88d98d8b1beb1f0bdfe95839daaf3ea50769f54
sabashahid786 <sabashahid4846@gmail.com>
2026-08-03 17:55:33 +0500

    6to9

---
+++ assignment6-9/app.js
@@ -0,0 +1,49 @@
+  
+//   Question-01
+  
+  document.write("Result:<br><br>");
+ var num = 10
+ document.write("The value of a is: " + num + "<br><br><br>");
+ "<br>"
+
+  document.write("The value of ++a is: " + (num + 1) + "<br>");
+   document.write("Now the value of a is: " + (num + 1) + "<br><br>");
+
+    document.write("The value of ++a is: " + (num + 1) + "<br>");
+     document.write("Now the value of a is: " + (num + 1 + 1) + "<br><br>");
+
+      document.write("The value of --a is: " + (num + 1) + "<br>");
+        document.write("Now the value of a is: " + (num + 1) + "<br><br>");
+
+          document.write("The value of a-- is: " + (num + 1) + "<br>");
+             document.write("Now the value of a is: " + (num) + "<br><br>");
+
+
+
+             //   Question-02
+              var a = 2
+              var  b = 1
+
+              console.log (--a)
+              console.log(--a - --b)
+
+           console.log( --a - --b + ++b) 
+          console.log(--a - --b + ++b + b-- ) 
+
+    var result = --a - --b + ++b + b--;
+
+    document.write("a is " + a + "<br>");
+    document.write("b is " + b + "<br>");
+    document.write("result is " + result);
+ "<br><br>"
+    
+
+
+
+
+      //   Question-03
+       var userName = prompt("Enter your name:");
+
+    alert("Welcome " + userName + "!");
+
+     
