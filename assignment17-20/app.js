a70efdbf55dd2471f0ad45088bb27b60d9fa5cd7
sabashahid786 <sabashahid4846@gmail.com>
2026-08-30 22:50:23 +0500

    assignment

---
+++ assignment17-20/app.js
@@ -0,0 +1,134 @@
+
+// Question-01
+// var group = [
+//     [    ,      ]
+//     [    ,      ]
+// ]
+
+
+// Question-02
+// var num = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ]
+// console.log(num)
+
+// Question-03
+
+// for ( var i=0; i<=10; i++) {
+//  console.log(i)
+// }
+
+// Question-04
+
+// var num = prompt("Table number?");
+// var length = prompt("Table length?");
+
+// for (var i=0; i<=length; i++){
+// console.log(num + " × " + i + " = " + (num * i));
+// }
+
+
+// Question-05
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"]
+// console.log(fruits)
+// document.write("</br>")
+
+
+// Question-06
+// document.write("Counting")
+// document.write(" ")
+// for (var i = 1; i <= 15; i++) {
+//     document.write(i + ", ");
+// }
+
+// document.write("</br>")
+
+// document.write(" Reverse Counting")
+// document.write(" ")
+// for (var i=10; i>=1; i--)(
+//     document.write(i + ", ")
+// )
+
+// document.write("</br>")
+
+// document.write(" Even Counting")
+// document.write(" ")
+// for (var i=0; i<=20; i+=2)(
+//     document.write(i + ", ")
+// )
+
+
+// document.write("</br>")
+
+// document.write(" Odd Counting")
+// document.write(" ")
+// for (var i=1; i<=19; i+=2)(
+//     document.write(i + ", ")
+// )
+
+
+
+// document.write("</br>")
+
+// document.write(" Series")
+// document.write(" ")
+
+// for (var i = 2; i <= 20; i += 2) {
+//     document.write(i + "k, ");
+// }
+
+// Question-07
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var item = prompt("What do you want to search?");
+// var found = false;
+// for (var i = 0; i < A.length; i++) {
+
+//     if (A[i] === item) {
+//         found = true;
+//         break;
+//     }
+
+// }
+// if (found) {
+//     alert("Item found");
+// }
+// else {
+//     alert("Item not found");
+// }
+// Question-08
+// var A = [24, 53, 78, 91, 12];
+
+// var largest = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+
+//     if (A[i] > largest) {
+//         largest = A[i];
+//     }
+
+// }
+
+// console.log(largest);
+
+// Question-09
+
+// var A = [24, 53, 78, 91, 12];
+
+// var smallest = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+
+//     if (A[i] < smallest) {
+//         smallest = A[i];
+//     }
+
+// }
+
+// console.log(smallest);
+
+// Question-10
+
+// for (var i = 5; i <= 100; i += 5) {
+//     console.log(i);
