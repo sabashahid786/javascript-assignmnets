9c1f7001bd57127b3bdf56891890544c8b4387f6
sabashahid786 <sabashahid4846@gmail.com>
2026-09-07 22:30:41 +0500

    assignment

---
+++ assignment26-30/app.js
@@ -0,0 +1,56 @@
+// question-01
+// var num = prompt("Enter a positive integer:");
+
+// num = Number(num);
+
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num));
+
+// question-02
+// var num = prompt("Enter a negative integer:");
+
+// num = Number(num);
+
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num));
+
+// question-03
+// var num = prompt("Enter a number:");
+// num = Number(num);
+
+// document.write("Number: " + num + "<br>");
+// document.write("Absolute value: " + Math.abs(num));
+
+// question-04
+// var dice = Math.floor(Math.random() * 6) + 1;
+
+// document.write("Dice value: " + dice);
+
+// question-05
+// var coin = Math.floor(Math.random() * 2);
+
+// if (coin === 0) {
+//     document.write("Heads");
+// } else {
+//     document.write("Tails");
+// }
+// question-06
+// var num = Math.floor(Math.random() * 100) + 1;
+
+// document.write("Random number: " + num);
+
+// question-07
+
+// question-08
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// var userNumber = prompt("Guess a number between 1 and 10:");
+// userNumber = Number(userNumber);
+
+// if (userNumber === secretNumber) {
+//     alert("Congratulations! You guessed the secret number.");
+// }
